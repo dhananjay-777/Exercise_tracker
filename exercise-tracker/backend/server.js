0e32e3e5316ede957a3f67e9968e8abd113bcfa3
dhananjay-777 <iit2020114@iiita.ac.in>
2023-08-10 17:23:18 +0530

    added

---
+++ exercise-tracker/backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
